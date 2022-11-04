0f0a47369b94b38b2a05a835091e3ed3cb807d41
j8won <joowoni99@gmail.com>
2022-11-04 21:37:03 +0900

    feat: Add INITIAL_RENTEE_INFO.js

---
+++ src/constant/INITIAL_RENTEE_INFO.js
@@ -0,0 +1,12 @@
+const INITIAL_RENTEE_INFO = {
+  id: "",
+  thumbnail: "",
+  title: "",
+  author: "",
+  publisher: "",
+  publishedDay: "",
+  note: "",
+};
+
+Object.freeze(INITIAL_RENTEE_INFO);
+export default INITIAL_RENTEE_INFO;
