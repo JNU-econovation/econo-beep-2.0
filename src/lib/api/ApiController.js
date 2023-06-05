6ff3190e1206ee6d912d3c0ff8f16e82a97b3e61
j8won <joowoni99@gmail.com>
2023-06-05 12:08:11 +0900

    feat: cookie를 활용한 토큰 재발급

---
+++ src/lib/api/ApiController.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { getCookie } from "../utils/cookie";
+
+const apiController = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_API,
+});
+
+apiController.interceptors.request.use(
+  function (config) {
+    config.headers["Content-Type"] = "application/json";
+    config.headers["Authorization"] = `Bearer ${localStorage?.getItem(
+      "accessToken"
+    )}`;
+    return config;
+  },
+  function (error) {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+apiController.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async function (err) {
+    const originalConfig = err.config;
+    if (err.response && err.response.status === 401) {
+      try {
+        await refreshAccessToken();
+        return apiController.request(originalConfig);
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    }
+    return Promise.reject(err);
+  }
+);
+
+const refreshAccessToken = async () => {
+  const response = await axios({
+    url: import.meta.env.VITE_ECONO_AUTH + "/api/accounts/cookie/re-issue",
+    method: "GET",
+    headers: { Authorization: `Bearer ${getCookie("refresh_token")}` },
+  });
+
+  localStorage.setItem("accessToken", response.data.access);
+};
+
+export default apiController;
